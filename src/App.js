906d5e67e52fedaccdb6b136e46802060785c1c9
rashad <88219300+rashadraz@users.noreply.github.com>
2022-12-01 19:14:03 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [count , setCount] = useState(0)
+
+  function increment(){
+    setCount(count+1)
+  }
+  function decrement(){
+    setCount(count-1)
+  }
+  return (
+    <div className="App">
+      <h1>{count}</h1>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+    </div>
+  );
+}
+
+export default App;
